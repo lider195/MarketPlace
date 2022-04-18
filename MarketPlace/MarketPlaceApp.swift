2fb31eff5e42a7c5bf2aafaff449da8fb25e1b01
lider <vadim4ik9@gmail.com>
2022-04-18 23:42:49 +0300

    initial commit

---
+++ MarketPlace/MarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPlaceApp.swift
+//  MarketPlace
+//
+//  Created by Vadim on 18.04.22.
+//
+
+import SwiftUI
+
+@main
+struct MarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
